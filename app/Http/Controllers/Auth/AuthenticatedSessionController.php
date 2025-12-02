0d4c79e05d2f8d541c9d0ad69e4aa7fbe164bd43
3041TAN-08 <3041TAN-08@tanuzem.boronkay.local>
2025-12-02 13:54:35 +0100

    Download breeze

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Display the login view.
+     */
+    public function create(): View
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $response = Http::api()->post('/user/login', [
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        if ($response->successful()) {
+			// elmentjük a bejelentkezési adatokat a session-be.
+            $token = $response['token']; 
+            $user = $response['user'];
+            session([
+                'api_token' => $token,
+                'user_name' => $user['name'],
+				'user_email' => $user['email'],
+            ]);
+
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors([
+            'email' => 'Hibás bejelentkezési adatok.',
+        ]);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        session()->forget('api_token');
+
+        return redirect('/');
+    }
+}
